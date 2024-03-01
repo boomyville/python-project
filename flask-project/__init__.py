e51c7e4ffa2c240d3422aaa57ad1006fedc62722
boomyville <boomyville@gmail.com>
2024-03-01 16:52:32 +1100

    Update __init__.py

--- flask-project/__init__.py
+++ flask-project/__init__.py
@@ -8,7 +8,8 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_mapping(SECRET_KEY=os.environ.get('SECRET_KEY', default='dev'))
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)  # Silent means nothing will be mentioned about this command 10     else:
+        app.config.from_pyfile('config.py', silent=True)  # Silent means nothing will be mentioned about this command 10     
+    else:
         app.config.from_mapping(test_config)
     
     from models import db
