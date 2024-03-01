d7ba08720be91fd06a0ed4c6d4603a48fd2cdb8a
boomyville <boomyville@gmail.com>
2024-03-02 00:36:47 +1100

    Update app.py

--- flask-project/app.py
+++ flask-project/app.py
@@ -25,3 +25,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mariadb+mariadbconnector://{user}:{pas
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ip = db.Column(db.Integer)
+    time = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    score = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Player {self.name}>'
