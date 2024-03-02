8027459e9cf84612ed381f470693b4daf9fdfe4d
boomyville <boomyville@gmail.com>
2024-03-02 12:14:39 +1100

    Update app.py

--- flask-project/app.py
+++ flask-project/app.py
@@ -26,7 +26,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Score(db.Model):
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     ip = db.Column(db.Integer)
